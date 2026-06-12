5e5ae62ab2902c97dc7d3a4514ec255ae9109501
Keyur Patel <pkeyur5175@gmail.com>
2026-06-12 11:58:02 +0530

    Control Flow Statement

---
+++ Assignment/P8.cpp
@@ -0,0 +1,27 @@
+/*
+    => Write a C++ program that takes a student’s marks as input and calculates the grade based on if-else conditions.
+*/
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int marks;
+
+    cout << "Enter student's marks: ";
+    cin >> marks;
+
+    if (marks >= 90)
+        cout << "Grade A";
+    else if (marks >= 80)
+        cout << "Grade B";
+    else if (marks >= 70)
+        cout << "Grade C";
+    else if (marks >= 60)
+        cout << "Grade D";
+    else
+        cout << "Grade F";
+
+    return 0;
+}
