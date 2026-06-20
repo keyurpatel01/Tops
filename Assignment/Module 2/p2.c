2f305955eb010e641c0ec51beae44d3b5c6f8f18
Keyur Patel <pkeyur5175@gmail.com>
2026-06-20 14:58:47 +0530

    Basic Hello World & Variables

---
+++ Assignment/Module 2/p2.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main()
+{
+    int age = 20;
+    char grade = 'A';
+    float percentage = 85.5;
+
+    const float PI = 3.14159;
+
+     printf("Age = %d\n", age);
+    printf("Grade = %c\n", grade);
+    printf("Percentage = %.2f\n", percentage);
+    printf("PI = %.5f\n", PI);
+
+    return 0;
+}
