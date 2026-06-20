918391fda8b2e91fd9f1f5558e9d0ff96a957249
Keyur Patel <pkeyur5175@gmail.com>
2026-06-20 15:36:50 +0530

    Grade Calculator

---
+++ Assignment/Module 2/p38.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main()
+{
+    float marks;
+
+    printf("Enter marks: ");
+    scanf("%f", &marks);
+
+    if (marks > 90)
+        printf("Grade A\n");
+    else if (marks > 75 && marks <= 90)
+        printf("Grade B\n");
+    else if (marks > 50 && marks <= 75)
+        printf("Grade C\n");
+    else
+        printf("Grade D\n");
+
+    return 0;
+}
