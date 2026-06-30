4e901383c9dec8d1da3134bf6d18960f0be511b2
Keyur Patel <pkeyur5175@gmail.com>
2026-06-30 14:52:18 +0530

    Matrix Addition in Array

---
+++ Assignment/Module 2/p52.c
@@ -0,0 +1,58 @@
+#include <stdio.h>
+
+int main()
+{
+    int a[3][3], b[3][3], result[3][3];
+    int i, j, k;
+
+    printf("Enter elements of first 3x3 matrix:\n");
+    for (i = 0; i < 3; i++)
+    {
+        for (j = 0; j < 3; j++)
+        {
+            scanf("%d", &a[i][j]);
+        }
+    }
+
+    printf("Enter elements of second 3x3 matrix:\n");
+    for (i = 0; i < 3; i++)
+    {
+        for (j = 0; j < 3; j++)
+        {
+            scanf("%d", &b[i][j]);
+        }
+    }
+
+    // Initialize result matrix
+    for (i = 0; i < 3; i++)
+    {
+        for (j = 0; j < 3; j++)
+        {
+            result[i][j] = 0;
+        }
+    }
+
+    // Matrix Multiplication
+    for (i = 0; i < 3; i++)
+    {
+        for (j = 0; j < 3; j++)
+        {
+            for (k = 0; k < 3; k++)
+            {
+                result[i][j] += a[i][k] * b[k][j];
+            }
+        }
+    }
+
+    printf("\nResultant Matrix:\n");
+    for (i = 0; i < 3; i++)
+    {
+        for (j = 0; j < 3; j++)
+        {
+            printf("%d ", result[i][j]);
+        }
+        printf("\n");
+    }
+
+    return 0;
+}
