ace20bfd02ecc89a7b600086b602cd2d099f13ac
Keyur Patel <pkeyur5175@gmail.com>
2026-07-21 20:47:38 +0530

    Flutter Project Structure & Widget Tree Fundamentals

--- Assignment/Flutter/flutter_assignment/lib/main.dart
+++ Assignment/Flutter/flutter_assignment/lib/main.dart
@@ -1,14 +1,21 @@
 import 'package:flutter/material.dart';
-
-import 'package:flutter_assignment/S3P4.dart';
+import 'package:flutter_assignment/S1P4.dart';
 
 void main() {
-  runApp(App());
+  runApp(const MyApp());
 }
 
-class App extends StatelessWidget {
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(home: S3P4(), debugShowCheckedModeBanner: false);
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        appBar: AppBar(title: const Text('My Profile')),
+        body: S1P4(),
+      ),
+    );
   }
 }
