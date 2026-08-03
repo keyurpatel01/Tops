b80b4244d7834fe3d47afb2aad9aaaa57f92868a
Keyur Patel <pkeyur5175@gmail.com>
2026-08-03 13:51:47 +0530

    Layout Widget:Row,Column, & Container

--- Assignment/Flutter/flutter_assignment/lib/main.dart
+++ Assignment/Flutter/flutter_assignment/lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_assignment/S1P4.dart';
+import 'package:flutter_assignment/S4P2.dart';
 
 void main() {
   runApp(const MyApp());
@@ -10,12 +10,6 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      home: Scaffold(
-        appBar: AppBar(title: const Text('My Profile')),
-        body: S1P4(),
-      ),
-    );
+    return MaterialApp(debugShowCheckedModeBanner: false, home: S4P2());
   }
 }
