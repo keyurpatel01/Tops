e4ab09b5919788dcde8a399d3b8d9cae2d93a1fc
Keyur Patel <pkeyur5175@gmail.com>
2026-08-03 16:26:37 +0530

    session 6

---
+++ Assignment/Flutter/flutter_assignment/lib/S6P2.dart
@@ -0,0 +1,6 @@
+/*
+  Build a simple playlist voting feature: show a song name and two buttons, 'Upvote' and '
+  Downvote'. When the user presses either button, update and display the current vote count in
+   real time using setState.<br><br><em><strong>Hint:</strong> Use onPressed for the buttons and 
+   update a voteCount variable inside setState.</em>
+*/
--- Assignment/Flutter/flutter_assignment/lib/main.dart
+++ Assignment/Flutter/flutter_assignment/lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_assignment/S5P5.dart';
+import 'package:flutter_assignment/S6P1.dart';
 
 void main() {
   runApp(const MyApp());
@@ -10,6 +10,6 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(debugShowCheckedModeBanner: false, home: S5P5());
+    return MaterialApp(debugShowCheckedModeBanner: false, home: S6P1());
   }
 }
